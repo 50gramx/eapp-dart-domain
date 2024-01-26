b6f66cfb290989d9438a7236f6ed9976eaecfc74
amit.khetan.70 <amit.khetan.70@50gramx.io>
2024-01-26 21:52:56 +0530

    fix domain

---
+++ src/eapp_dart_domain/example/eapp_dart_domain_example.dart
@@ -0,0 +1,6 @@
+import 'package:eapp_dart_domain/eapp_dart_domain.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ src/eapp_dart_domain/lib/eapp_dart_domain.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/eapp_dart_domain_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
